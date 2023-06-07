df454b7a77380658307731178b3a3515b084bc27
x <cechinel.a.k@gmail.com>
2023-06-07 17:42:02 +0300

    indoor data

--- yolo_dataset/train.py
+++ yolo_dataset/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("best.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-results = model.train(data="train.yaml", epochs=500)  # train the model
+results = model.train(data="/home/x/Documents/landing_pipeline/yolo_dataset/indoor/train.yaml", epochs=500)  # train the model
