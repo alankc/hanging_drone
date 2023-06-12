c6a05cd2069073ec6516c592eae827e3ce8395d4
x <cechinel.a.k@gmail.com>
2023-06-12 10:19:35 +0300

    first version

--- communication.py
+++ communication.py
@@ -1,13 +1,14 @@
 import socket
 
-__READY = 0
-__FAIL = 1
+LAND_REQUEST = "LANDR"
+READY = "READY"
+TAKEOFF = "TAKEOFF"
+FAIL = "FAIL"
 
-class RechargeStationComm:
-
+class Server:
     def __init__(self, host, port) -> None:
-        self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
+            self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.__s.bind((host, port))
             self.__s.listen()
         except Exception as e:
@@ -15,7 +16,7 @@ class RechargeStationComm:
             self.__s.close()
 
     #None timeout = Infinitum time, loop = 1, runs only once
-    def get_message(self, timeout = None, loop = 1):
+    def wait_conn(self, timeout = None, loop = 1):
         self.__s.settimeout(timeout)
         while loop > 0:
             loop = loop - 1
@@ -23,11 +24,6 @@ class RechargeStationComm:
                 conn, addr = self.__s.accept()
                 self.__curr_conn = conn
                 self.__curr_addr = addr
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-
             except Exception as e:
                 if not isinstance(e, TimeoutError):
                     conn.close()
@@ -35,15 +31,86 @@ class RechargeStationComm:
                     self.__curr_addr = None
                     print("Error getting message" + e)
 
-    def send_response(self, response = __READY):
+    def receive_msg(self, type = None):
         try:
-            self.__curr_conn.sendall(__READY)
+            data = ""
+            while True:
+                d = self.__curr_conn.recv(1024)
+                if not d: break
+                data = data + str(d)
+
+            if not type: 
+                return data
+            else:
+                return type in data
+
         except Exception as e:
+            self.__curr_conn.close()
+            self.__curr_conn = None
+            self.__curr_addr = None
             print("Error getting message" + e)
+
+    def send_msg(self, msg = READY):
+        try:
+            self.__curr_conn.sendall(msg)
+        except Exception as e:
+            print("Error sending message" + e)
         finally:
-            self.__curr_conn.close()    
+            self.__curr_conn.close()
+
+    def close_curr(self):
+        self.__curr_conn.close()
+        self.__curr_conn = None
+        self.__curr_addr = None
+
+    def close(self):
+        self.__s.close()
 
 
-class DroneComm:
-    def __init__(self) -> None:
-        pass
+class Client:
+    def __init__(self, host:str, port:int) -> None:
+        self.__host = host
+        self.__port = port
+
+    def conn(self):
+        try:
+            self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__s.connect(self.__host, self.__port)
+        except Exception as e:
+            print("Error sending message" + e)
+            self.__s.close()     
+
+    def send_msg(self, msg = LAND_REQUEST):
+        try:
+            self.__s.sendall(msg)
+        except Exception as e:
+            print("Error sending message" + e)
+            self.__s.close()              
+
+    def receive_msg(self, type = None):
+        try:
+            data = ""
+            while True:
+                d = self.__s.recv(1024)
+                if not d: break
+                data = data + str(d)
+
+            if not type: 
+                return data
+            else:
+                return type in data
+            
+        except Exception as e:
+            self.__s.close()
+            self.__s = None
+            print("Error getting message" + e)
+    
+    def close(self):
+        self.__s.close()
+
+    def is_type(self, data, type = [LAND_REQUEST, READY, TAKEOFF, FAIL]):
+        return type in data
+
+if __name__ == "__main__":
+    c = Client("", 2810)
+    c.is_type("asdadsa", LAND_REQUEST)
