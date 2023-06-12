d3fbcd7ba67be8af0e93700ef529782fa3c4d2f9
x <cechinel.a.k@gmail.com>
2023-06-12 11:57:08 +0300

    only printing unespected exceptions

--- communication.py
+++ communication.py
@@ -12,9 +12,9 @@ class Server:
             self.__s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.__s.bind((host, port))
             self.__s.listen()
+            
         except Exception as e:
             print("Error initializing the server: " + str(e))
-            self.__s.close()
 
     #None timeout = Infinitum time, loop = 1, runs only once
     def wait_conn(self, timeout = None, loop = 1):
@@ -25,11 +25,9 @@ class Server:
                 conn, addr = self.__s.accept()
                 self.__curr_conn = conn
                 self.__curr_addr = addr
+
             except Exception as e:
                 if not isinstance(e, TimeoutError):
-                    conn.close()
-                    self.__curr_conn = None
-                    self.__curr_addr = None
                     print("Error connection " + str(e))
                     return False
         return True
@@ -43,9 +41,6 @@ class Server:
                 return type in data
 
         except Exception as e:
-            self.__curr_conn.close()
-            self.__curr_conn = None
-            self.__curr_addr = None
             print("Error getting message " + str(e))
             return False
 
@@ -53,6 +48,7 @@ class Server:
         try:
             self.__curr_conn.sendall(msg.encode())
             return True
+        
         except Exception as e:
             print("Error sending message " + str(e))
             return False
@@ -76,18 +72,18 @@ class Client:
             self.__s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.__s.connect((self.__host, self.__port))
             return True
+        
         except Exception as e:
             print("Error connecting " + str(e))
-            self.__s.close()
             return False
 
     def send_msg(self, msg = LAND_REQUEST):
         try:
             self.__s.sendall(msg.encode())
             return True
+        
         except Exception as e:
             print("Error sending message " + str(e))
-            self.__s.close()
             return False       
 
     def receive_msg(self, type = None):
@@ -100,8 +96,6 @@ class Client:
                 return type in data
             
         except Exception as e:
-            self.__s.close()
-            self.__s = None
             print("Error getting message " + str(e))
             return False
     
