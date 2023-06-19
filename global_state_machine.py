e6055b2244b0452ebbcb20f362b66c1a791a644b
x <cechinel.a.k@gmail.com>
2023-06-19 14:27:22 +0300

    v0

--- global_state_machine.py
+++ global_state_machine.py
@@ -12,28 +12,43 @@ from communication import D2RS
 
 class GlobalStateMachine:
     S_DISCONECTED = 0
-    S_WAITING_RS = 1
-    S_MANUAL = 2
-    S_AUTONOMOUS = 3
+    S_WAITING_RS_TAKEOFF = 1
+    S_WAITING_RS_LAND = 2
+    S_MANUAL = 3
+    S_MANUAL_LAND = 4
+    S_AUTONOMOUS = 5
 
     def __init__(self, parameters:dict) -> None:
         self.__parameters = parameters
         self.__select_rect = []
         self.__state = self.S_DISCONECTED
+        self.__lp = None
 
     def mouse_click(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.__select_rect.append([x,y])
 
     def state_disconected(self):
-        self.state_waiting_rs()
+        print("*************************************************")
+        print("************* TRYING CONNECTION *****************")
+        print("*************************************************", flush=True)
+        self.state_waiting_rs_takeoff()
         if self.__state == self.S_DISCONECTED and self.__d2rs.wifi_conect(None):
             print("Ready to takeoff")
             self.__state = self.S_MANUAL
             self.__ed.connect()
             self.__ed.start()
 
-    def state_waiting_rs(self):
+    def state_waiting_rs_takeoff(self):
+        
+        frame = np.zeros((720, 960, 3), dtype = np.uint8)
+        ut.draw_big_text(frame, "WAITING CONNECTION")
+
+        cv2.imshow('Camera', frame)
+        key = cv2.waitKey(30) & 0xFF
+        if key == 27:
+            exit(0)
+
         res_ssid = self.__d2rs.takeoff_request(0.5)
         if res_ssid:
             count = 5
@@ -51,11 +66,168 @@ class GlobalStateMachine:
                 self.__ed.connect()
                 self.__ed.start()
 
+    def state_waiting_rs_land(self):
+        
+        frame = np.zeros((720, 960, 3), dtype = np.uint8)
+        ut.draw_big_text(frame, "WAITING LAND PERMISSION")
+
+        cv2.imshow('Camera', frame)
+        key = cv2.waitKey(30) & 0xFF
+        if key == 27:
+            self.__ed.land()
+            time.sleep(5)
+            self.__ed.quit()
+            self.__select_rect = []
+            exit(0)
+        elif key  == ord(" "):
+            self.__state = self.S_MANUAL
+            return
+
+        result = self.__d2rs.land_request(0.2)
+        if result:
+            self.__state = self.S_MANUAL_LAND
+
     def state_manual(self):
-        pass
+        
+        frame = self.__ed.get_curr_frame()
+            
+        if frame is None:
+            image = np.zeros((720, 960, 3), dtype = np.uint8)
+            ut.draw_big_text(image, "WAITING FRAME")
+            self.__select_rect = []
+            key = cv2.waitKey(30) & 0xFF
+            if key == 27:
+                self.__ed.quit()
+                exit(0)
+            return
+            
+        elif (len(self.__select_rect) < 1):
+            image = self.__s.rotateImage(frame)
+                    
+        image_s = image.copy()
+
+        if (len(self.__select_rect) > 2):
+            ut.draw_polylines(image_s, [np.array(self.__select_rect)])
+
+        ut.draw_text(image_s, f"FPS={self.__fps:.1f}         MANUAL CONTROL", -1)
+        cv2.imshow('Camera', image_s)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            self.__ed.land()
+            time.sleep(5)
+            self.__ed.quit()
+            self.__select_rect = []
+            exit(0)
+
+        elif key == ord("q"):
+            self.__ed.land()
+            print("*************************************************")
+            print("******************** LAND ***********************")
+            print("*************************************************", flush=True)
+
+        elif key == ord("e"):
+            self.__ed.takeoff()
+            print("*************************************************")
+            print("****************** TAKE OFF *********************")
+            print("*************************************************", flush=True)
+
+        elif key == ord("1"): # Use YOLO
+            self.__lp = LandingPipeline(self.__ed, self.__s, self.__v, self.__yd, None, None, int(round(self.__cx, 0)), int(round(self.__cy, 0)), self.__out_file)
+            self.__ed.PID_reset() #reseting all PIDs
+            self.__state = self.S_AUTONOMOUS
+
+        elif key == ord("2") and len(self.__select_rect) > 2: # USe selected rectangle
+            k_ref, d_ref = self.__v.detect_features_in_polygon(image, np.array(self.__select_rect))
+            self.__select_rect = []
+            self.__lp = LandingPipeline(self.__ed, self.__s, self.__v, self.__yd, k_ref, d_ref, int(round(self.__cx, 0)), int(round(self.__cy, 0)), self.__out_file)
+            self.__ed.PID_reset() #reseting all PIDs
+            self.__state = self.S_AUTONOMOUS
+
+        elif key == ord("3"):# Clear selected rectangle
+            self.__select_rect = []
+
+        elif key == ord("m"):# Manual land
+            self.__state = self.S_WAITING_RS_LAND
+
+        elif key == ord(" "):# Clear selected rectangle
+            if not (self.__lp is None):
+                self.__ed.rc_control() #STOPING all controllers
+                self.__ed.PID_reset() #reseting all PIDs
+                self.__state = self.S_AUTONOMOUS
+        else:
+            ut.rc_control(key, self.__ed)
+
+    def state_manual_land(self):
+        
+        frame = self.__ed.get_curr_frame()
+            
+        if frame is None:
+            image = np.zeros((720, 960, 3), dtype = np.uint8)
+            ut.draw_big_text(image, "WAITING FRAME")
+            self.__select_rect = []
+            key = cv2.waitKey(30) & 0xFF
+            if key == 27:
+                self.__ed.quit()
+                exit(0)
+            return
+            
+        image = self.__s.rotateImage(frame)        
+        image_s = image.copy()
+
+        ut.draw_text(image_s, f"FPS={self.__fps:.1f}         MANUAL LAND", -1)
+        cv2.imshow('Camera', image_s)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            self.__ed.land()
+            time.sleep(5)
+            self.__ed.quit()
+            self.__select_rect = []
+            exit(0)
+
+        elif key == ord("q"):
+            self.__ed.land()
+            print("*************************************************")
+            print("******************** LAND ***********************")
+            print("*************************************************", flush=True)
+
+        elif key == ord("Q"):
+            self.__ed.quit()
+            self.__state = self.S_WAITING_RS_TAKEOFF
+
+        elif key == ord(" "):# Clear selected rectangle
+                self.__state = self.S_MANUAL
+        else:
+            ut.rc_control(key, self.__ed)
 
     def state_autonomous(self):
-        pass
+        frame = self.__ed.get_curr_frame()
+        image = self.__s.rotateImage(frame)
+        image_s = image.copy()
+
+        result = lp.run(image, image_s)
+        if (result == lp.SUCESS) or (result == lp.FAIL):
+            self.__state = self.S_MANUAL
+            lp = None
+            self.__ed.rc_control() #STOPING all controllers
+
+        ut.draw_text(image_s, f"FPS={self.__fps:.1f}", -1)
+        cv2.imshow('Camera', image_s)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            self.__ed.land()
+            time.sleep(5)
+            self.__ed.quit()
+            self.__select_rect = []
+            exit(0)
+
+        elif key == ord(" "):# Change to manual control
+            self.__lp.reset()
+            self.__ed.rc_control() #STOPING all controllers
+            self.__ed.PID_reset() #reseting all PIDs
+            self.__state = self.S_MANUAL
 
     def start(self):
         self.__ed = EasyDrone(True, self.__parameters['Camera']['stream'], self.__parameters['Control']['pid'])
@@ -73,10 +245,10 @@ class GlobalStateMachine:
         self.__s = Stereo()
         sp = self.__parameters['Camera']
         self.__s.set_camera_params(sp['fx'], sp['fy'], sp['pitch'], sp['cx'], sp['cy_aligned'])
-        cx = sp['cx']
-        cy = sp['cy_aligned']
+        self.__cx = sp['cx']
+        self.__cy = sp['cy_aligned']
 
-        out_file = self.__parameters['Control']['folder_odom']
+        self.__out_file = self.__parameters['Control']['folder_odom']
 
         self.__yd = YOLODetector(self.__parameters['YOLO']['path'])
 
@@ -92,96 +264,29 @@ class GlobalStateMachine:
 
         time_start = time.time()
         alpha = 0.1
-        fps = 0
-
-        self.__ed.connect()
-        self.__ed.start()
+        self.__fps = 0
 
         while True:
-            frame = self.__ed.get_curr_frame()
-            
-            if frame is None:
-                    image = np.zeros((720, 960, 3), dtype = np.uint8)
-                    self.__select_rect = []
-                    key_delay = 30
-            
-            elif (len(self.__select_rect) < 1):
-                    image = self.__s.rotateImage(frame)
-                    key_delay = 1
-                    
-            image_s = image.copy()
+            curr_state = self.__state
+            if curr_state == self.S_DISCONECTED:
+                self.state_disconected()
 
-            if not (frame is None) and (len(self.__select_rect) > 2) and manual_control:
-                ut.draw_polylines(image_s, [np.array(self.__select_rect)])
+            if curr_state == self.S_WAITING_RS_TAKEOFF:
+                self.state_waiting_rs_takeoff()
 
-            if not manual_control:
-                #if not (lp is None):
-                result = lp.run(image, image_s)
-                if (result == lp.SUCESS) or (result == lp.FAIL):
-                    manual_control = True
-                    lp = None
-                    self.__ed.rc_control() #STOPING all controllers
-                #else:
-                    #print("Press 1 to run landing pipeline with Yolo or")
-                    #print("Press 2 to run landing pipeline after selecting landing site")
-                    #manual_control = True
-                
+            if curr_state == self.S_WAITING_RS_LAND:
+                self.state_waiting_rs_land()
+
+            if curr_state == self.S_MANUAL_LAND:
+                self.state_manual_land()
+
+            if curr_state == self.S_MANUAL:
+                self.state_manual()
+
+            if curr_state == self.S_AUTONOMOUS:
+                self.state_autonomous()
+
             if time.time() - time_start > 0:
-                fps = (1 - alpha) * fps + alpha * 1 / (time.time()-time_start)  # exponential moving average
+                self.__fps = (1 - alpha) * self.__fps + alpha * 1 / (time.time()-time_start)  # exponential moving average
                 time_start = time.time()
             
-            if manual_control:
-                ut.draw_text(image_s, f"FPS={fps:.1f}         MANUAL CONTROL", -1)
-            else:
-                ut.draw_text(image_s, f"FPS={fps:.1f}", -1)
-
-            cv2.imshow('Camera', image_s)
-
-            key = cv2.waitKey(key_delay) & 0xFF
-            if key == 27:
-                self.__ed.land()
-                time.sleep(5)
-                self.__ed.quit()
-                self.__select_rect = []
-                exit(0)
-
-            elif key == ord("q"):
-                self.__ed.land()
-                print("*************************************************")
-                print("******************** LAND ***********************")
-                print("*************************************************", flush=True)
-
-            elif key == ord("e"):
-                self.__ed.takeoff()
-                print("*************************************************")
-                print("****************** TAKE OFF *********************")
-                print("*************************************************", flush=True)
-
-            elif key == ord("1"): # Use YOLO
-                manual_control = False
-                lp = LandingPipeline(self.__ed, self.__s, self.__v, self.__yd, None, None, int(round(cx, 0)), int(round(cy, 0)), out_file)
-                self.__ed.PID_reset() #reseting all PIDs
-
-            elif key == ord("2") and len(self.__select_rect) > 2: # USe selected rectangle
-                manual_control = False
-                k_ref, d_ref = self.__v.detect_features_in_polygon(image, np.array(self.__select_rect))
-                self.__select_rect = []
-                lp = LandingPipeline(self.__ed, self.__s, self.__v, self.__yd, k_ref, d_ref, int(round(cx, 0)), int(round(cy, 0)), out_file)
-                self.__ed.PID_reset() #reseting all PIDs
-
-            elif key == ord("3"):# Clear selected rectangle
-                self.__select_rect = []
-
-            elif key == ord(" "):# Change to manual control
-                if not (lp is None):
-                    manual_control = not manual_control
-                    lp.reset()  
-                else:
-                    manual_control = True
-
-                self.__ed.rc_control() #STOPING all controllers
-                self.__ed.PID_reset() #reseting all PIDs
-
-            elif manual_control: 
-                ut.rc_control(key, self.__ed)
-        
