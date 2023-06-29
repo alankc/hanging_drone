be8f959abbba9a0e3c234c79a9c95bf9dce047a6
Alan <cechinel.a.k@gmail.com>
2023-06-29 13:44:39 +0300

    new training

--- yolo_dataset/train.py
+++ yolo_dataset/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 #args https://docs.ultralytics.com/modes/train/#arguments
 # Load a model
-model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)YOLO("yolov8n.yaml")
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)YOLO("yolov8n.yaml")
 
 # Use the model
-results = model.train(data="/home/x/Documents/landing_pipeline/yolo_dataset/mixed/train.yaml", epochs=1000, patience=200)  # train the model
+results = model.train(data="/home/alan/Documentos/landing_pipeline/yolo_dataset/indoor/train.yaml", epochs=1000, patience=200)  # train the model
