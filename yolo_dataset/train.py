744a0aa570ce87bceb949340b0646bce02ac01d2
x <cechinel.a.k@gmail.com>
2023-07-28 10:37:25 +0300

    train and validation for artificial branch

--- yolo_dataset/train.py
+++ yolo_dataset/train.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 #args https://docs.ultralytics.com/modes/train/#arguments
 
-train_path = "/home/x/Documents/landing_pipeline/yolo_dataset/indoor/train.yaml"
+train_path = "/home/x/Documents/landing_pipeline/yolo_dataset/ab/train.yaml"
 
 # Load a model --------> train 9
-model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)YOLO("yolov8n.yaml")
+model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)YOLO("yolov8n.yaml")
 # Use the model
-results = model.train(data=train_path, epochs=1000, patience=200)  # train the model
+results = model.train(data=train_path, epochs=5000, patience=200)  # train the model
