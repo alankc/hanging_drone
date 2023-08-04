6c582dd1c2516d15b2bff61f1a455ec77e5ee7d5
x <cechinel.a.k@gmail.com>
2023-08-04 15:18:03 +0300

    natural branch dataset

--- yolo_dataset/train.py
+++ yolo_dataset/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 #args https://docs.ultralytics.com/modes/train/#arguments
 
-train_path = "/home/x/Documents/landing_pipeline/yolo_dataset/nb/train.yaml"
+train_path = "/home/x/Documents/landing_pipeline/yolo_dataset/natural_branch/train.yaml"
 
 
 model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)YOLO("yolov8n.yaml")
